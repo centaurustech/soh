b68ee8daf73c09e240b1224994e10709d9f87afa
davidemontorio <funky.davide@gmail.com>
2015-06-16 09:19:41 +0200

    Changes
    1. New Logo
    2. Scroll down button
    3. Tried to fix smooth scrolling
    4. Style improvement
    5. Removed some unused files

--- script/scrolling-nav.js
+++ script/scrolling-nav.js
@@ -1,18 +1,40 @@
 $(document).ready(function () {
-    // === Cover Image
+    var currentItem = {title: 'Il Teatro', color:'orange-text'};
+    $('#menu-title').html(currentItem.title).addClass(currentItem.color).hide();
+    
+    // === Navigation Affix
     var navPosition = $('.navbar').offset().top;
     $(window).scroll(function () {
-        if ($(window).scrollTop() >= navPosition) {
+        if ($(window).scrollTop() > navPosition) {
             $('.navbar').addClass("navbar-fixed-top");
         } else {
             $('.navbar').removeClass("navbar-fixed-top");
         }
     });
-    // === Smooth Scrolling
-    $('.navbar .navbar-nav li>a').bind('click', function (event) {
-        var link = $(this).attr('href');
-        var posi = $(link).offset().top;
-        $('body,html').animate({scrollTop: posi}, 1000, 'easeInOutExpo');
+    
+    // === Smooth Scrolling and Custom Menu
+    $('body').on('click', '#scroll-down-link', function(event){
+        $('.menu-teatro').click();
         event.preventDefault();
+    });
+    
+    $('.navbar .navbar-nav li>a').bind({
+        click: function (event) {
+            var link            = $(this).attr('href');
+            var posi            = $(link).offset();
+            currentItem.title   = $(this).attr('title');
+            currentItem.color   = $(this).data('color');
+            $('#menu-title').removeClass().addClass(currentItem.color).html(currentItem.title);
+            $('html, body').animate({scrollTop: posi.top}, 1000, 'easeInOutExpo');
+            event.preventDefault();
+        },
+        mouseenter: function(){
+            var title   = $(this).attr('title');
+            var color   = $(this).data('color');
+            $('#menu-title').removeClass().addClass(color).html(title).show();
+        },
+        mouseleave: function(){
+            $('#menu-title').removeClass().addClass(currentItem.color).html(currentItem.title).hide();
+        }
     });
 });
