810c5f11a81111b9f246a9d252ed21b5c4a58d1f
davidemontorio <funky.davide@gmail.com>
2015-06-26 16:23:57 +0200

    CHI SIAMO

--- script/chisiamo.js
+++ script/chisiamo.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
         var name    = $(this).find('h1').html();
         var bio     = $(this).find('.bio').html();
         var picture = $(this).find('img').attr('src');
-        $('.modal-title').html(name);
+        var role    = $(this).find('h4').html();
+        $('.modal-title .name').html(name);
+        $('.modal-title .role').html(" - " + role);
         $('.modal-body figure img').attr('src', picture);
         $('.modal-body .modal_bio').html(bio);
         $('#modal').modal('show');
